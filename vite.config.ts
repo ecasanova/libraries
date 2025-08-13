9246d987b2692ccb2dfbb393f5ba4dae76c5cffb
Enrique Casanova <enrique.casanova@projekt202.com>
2025-08-13 14:25:53 -0500

    feat: Update Vite configuration to load environment variables dynamically

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,21 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    global: "globalThis",
-    "process.env": {
-      NEXTAUTH_URL: JSON.stringify(
-        process.env.NEXTAUTH_URL || "http://localhost:5174"
+export default defineConfig(({ mode }) => {
+  // Cargar las variables de entorno
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return {
+    plugins: [react()],
+    define: {
+      global: "globalThis",
+      "process.env.NEXTAUTH_URL": JSON.stringify(
+        env.NEXTAUTH_URL || "http://localhost:5174"
       ),
-      NEXTAUTH_SECRET: JSON.stringify(process.env.NEXTAUTH_SECRET),
-      GITHUB_CLIENT_ID: JSON.stringify(process.env.GITHUB_CLIENT_ID),
-      GITHUB_CLIENT_SECRET: JSON.stringify(process.env.GITHUB_CLIENT_SECRET),
+      "process.env.NEXTAUTH_SECRET": JSON.stringify(env.NEXTAUTH_SECRET),
+      "process.env.GITHUB_CLIENT_ID": JSON.stringify(env.GITHUB_CLIENT_ID),
+      "process.env.GITHUB_CLIENT_SECRET": JSON.stringify(env.GITHUB_CLIENT_SECRET),
+      "process.env.NODE_ENV": JSON.stringify(mode),
     },
-  },
+  };
 });
