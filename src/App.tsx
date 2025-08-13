0f89e44dc91fb111af595d89ce4459404d6136e9
Enrique Casanova <enrique.casanova@projekt202.com>
2025-08-13 14:03:14 -0500

    fix: Remove unwanted characters from recommended libraries and source code links

--- src/App.tsx
+++ src/App.tsx
@@ -214,7 +214,7 @@ export default function App() {
             </a>
           </p>
           <p>
-            � Bibliotecas recomendadas por{" "}
+            Bibliotecas recomendadas por{" "}
             <a
               href="https://github.com/midudev"
               target="_blank"
@@ -225,7 +225,7 @@ export default function App() {
             </a>
           </p>
           <p>
-            �📂 Código fuente disponible en{" "}
+            Código fuente disponible en{" "}
             <a
               href="https://github.com/ecasanova/libraries"
               target="_blank"
