730be6a7631f601a5bb1ed40548d4c82b12770df
Enrique Casanova <enrique.casanova@projekt202.com>
2025-08-13 12:36:45 -0500

    refactor: add type parameters to useDragAndDrop for better type safety

--- examples/drag-and-drop.tsx
+++ examples/drag-and-drop.tsx
@@ -1,7 +1,10 @@
 import { useDragAndDrop } from "@formkit/drag-and-drop/react";
 
 export default function Lista() {
-  const [ref, items] = useDragAndDrop(["Elemento 1", "Elemento 2"]);
+  const [ref, items] = useDragAndDrop<HTMLUListElement, string>([
+    "Elemento 1",
+    "Elemento 2",
+  ]);
 
   return (
     <ul ref={ref}>
